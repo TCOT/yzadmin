1e1910ab24bcd3fff3e06519c1e68b2603a0888a
yizheng <venus.vsg>
2020-05-09 14:40:28 +0800

    新增顶栏右侧操作

---
+++ src/layout/components/GlobalHeader/head-right-component.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="head-right-component">
+   <slot/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "head-right-component"
+  }
+</script>
+
+<style lang="scss" scoped>
+  .head-right-component {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    padding: 0 3px;
+    cursor: pointer;
+    transition: all .3s;
+
+    &:hover {
+      background-color: #f9f9f9;
+    }
+  }
+</style>
--- src/layout/components/GlobalHeader/head-right.vue
+++ src/layout/components/GlobalHeader/head-right.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="head-right">
-    <div class="head-right-component">
+    <head-right-component>
       <svg-icon icon-class="night"></svg-icon>
-    </div>
-    <div class="head-right-component">
+    </head-right-component>
+    <head-right-component>
       <svg-icon icon-class="night"></svg-icon>
-    </div>
-    <div class="head-right-component">
+    </head-right-component>
+    <head-right-component>
       <svg-icon icon-class="night"></svg-icon>
-    </div>
-    <div class="head-right-component">
+    </head-right-component>
+    <head-right-component>
       <svg-icon icon-class="night"></svg-icon>
-    </div>
+    </head-right-component>
+
   </div>
 </template>
 
 <script>
+  import HeadRightComponent from "@/layout/components/GlobalHeader/head-right-component";
+
   export default {
-    name: "head-right"
+    name: "head-right",
+    components: {HeadRightComponent}
   }
 </script>
 
@@ -30,16 +34,5 @@
     display: flex;
     align-items: center;
     padding-right: 15px;
-    .head-right-component{
-      display: flex;
-      align-items: center;
-      height: 100%;
-      padding: 0 3px;
-      cursor: pointer;
-      transition: all .3s;
-      &:hover{
-        background-color: #f9f9f9;
-      }
-    }
   }
 </style>
