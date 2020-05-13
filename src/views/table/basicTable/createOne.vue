e49e824699f874119daf0ea5aa906a0a3d4b8e44
yizheng <venus.vsg>
2020-05-13 11:13:32 +0800

    新增抽屉布局

--- src/views/table/basicTable/createOne.vue
+++ src/views/table/basicTable/createOne.vue
@@ -4,10 +4,8 @@
       <div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div><div>11111</div>
     </div>
     <div class="drawer-footer-container">
-      <div style="text-align: center">
         <el-button >取消</el-button>
-        <el-button type="primary" style="margin:0px 60px">提交</el-button>
-      </div>
+        <el-button type="primary">提交</el-button>
     </div>
   </div>
 </template>
